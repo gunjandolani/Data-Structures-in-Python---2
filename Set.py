374f2bd5d98e81cceef13843ad245dfc9d8832ff
gunjandolani <gunjan.dolani1@gmail.com>
2024-08-31 16:42:55 +0530

    first commit

---
+++ Set.py
@@ -0,0 +1,22 @@
+# x = {1 , 2 , 3 , 4 , 4 , 5 , 5 , 5 , 6}
+# print(x)
+x = {1 , 2 , 3 , 4 , 5}
+y = {2 , 5 , 8 , 0 , 3}
+# print("x =" ,x)
+# print("y = " ,y)
+# XintersectionY = x.intersection(y)
+# print("X intersection Y =" , XintersectionY)
+
+# print("x =" , x)
+# print("y =" , y)
+# XunionY = x.union(y)
+# print("X union Y =" , XunionY)
+
+print("x =" , x)
+print("y =" , y)
+XdifferenceY = x.difference(y)
+print("X difference Y =" , XdifferenceY)
+
+
+
+
---
+++ Tuple.py
@@ -0,0 +1,9 @@
+tuple2 = (4 , "Gunjan" , 3.14 , (5 , 27))
+print(tuple2[3][1])
+print(tuple2[1:3])
+tuple = ("G" , "u" , "n" , "j" , "a" , "n")
+for letter in tuple:
+    print("Hello" , letter)
+for item in tuple2:
+    print(item)
+
